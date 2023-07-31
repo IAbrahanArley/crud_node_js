d9c8a2e88a5401dd33240df95e611e9d66944da8
IAbrahanarley <abrahanarley96@gmail.com>
2023-07-31 10:11:15 -0300

    backend e frontend

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,29 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
+//DB
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
-sever.get('/', (req, res, next) => res.send('Backend'))
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.jason())
+server.use(cors())
+
+//ODM
+
+const Client = restful.model('client', {
+    name: {type: String, required: true}
+})
+
+// rest API
+
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+//routes
+Client.register(server, '/clients')
 
 server.listen(3000)
